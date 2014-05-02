7176a2b11e0594e6d7c54330f6739e295c726198
Shawn Allen <shawnallen@mac.com>
2014-05-02 15:21:42 -0500

    Completely remove Crashlytics from the GitHub version of the repository.

--- Sawyer/TSAppDelegate.m
+++ Sawyer/TSAppDelegate.m
@@ -13,10 +13,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#ifndef DEBUG
-    [Crashlytics startWithAPIKey:@"CRASHLYTICS_API_KEY"];
-#endif
-    
     // Override point for customization after application launch.
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
         UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
